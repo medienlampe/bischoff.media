4f15b729b0070aaf78be718c40759f4501ecce42
Sven Bischoff <sven@medienlampe.de>
2016-11-04 20:23:09 +0100

    Implements support for IndieWeb.org

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
   <meta name="description" content="Der Full-Stack Web-Entwickler Sven Bischoff stellt sich vor. Links zu seinen Blog und Lebenslauf, sowie den Profilen bei GitHub, Twitter, Facebook, Steam, XING und LinkedIn.">
   <link rel="author" href="humans.txt" />
   <style type="text/css" media="screen">
-    <?php 
+    <?php
       require_once('./assets/css/materialize.min.css');
       require_once('./assets/css/style.min.css');
     ?>
@@ -24,26 +24,26 @@
       <h3 id="sb-bscmedieninformatik"><span class="sb-scp-bold">B.Sc.</span> <span class="sb-scp-light">Medieninformatik</span></h3>
     </div>
     <div class="center-align">
-      <a href="https://multigeneris.org" class="sb-scp-button waves-effect waves-light btn blue-grey lighten-3 blue-grey-text text-darken-4"><span class="sb-socicon socicon-wordpress left"></span>Blog</a>
-      <a href="https://svenbischoff.de" class="sb-scp-button waves-effect waves-light btn blue-grey lighten-3 blue-grey-text text-darken-4"><span class="sb-socicon socicon-persona left"></span>Lebenslauf</a>
+      <a href="https://multigeneris.org" class="sb-scp-button waves-effect waves-light btn blue-grey lighten-3 blue-grey-text text-darken-4" rel="me"><span class="sb-socicon socicon-wordpress left"></span>Blog</a>
+      <a href="https://svenbischoff.de" class="sb-scp-button waves-effect waves-light btn blue-grey lighten-3 blue-grey-text text-darken-4" rel="me"><span class="sb-socicon socicon-persona left"></span>Lebenslauf</a>
     </div>
     <div class="center-align">
-      <a href="https://github.com/medienlampe" class="sb-scp-button waves-effect waves-light btn blue-grey lighten-3 blue-grey-text text-darken-4"><span class="sb-socicon socicon-github left"></span>GitHub</a>
-      <a href="https://twitter.com/TheTalesOfImmo" class="waves-effect waves-light btn blue-grey lighten-3 blue-grey-text text-darken-4"><span class="sb-socicon socicon-twitter left"></span>Twitter</a>
-      <a href="https://www.facebook.com/sven.bischoff.393" class="waves-effect waves-light btn blue-grey lighten-3 blue-grey-text text-darken-4"><span class="sb-socicon socicon-facebook left"></span>Facebook</a>
+      <a href="https://github.com/medienlampe" class="sb-scp-button waves-effect waves-light btn blue-grey lighten-3 blue-grey-text text-darken-4" rel="me"><span class="sb-socicon socicon-github left"></span>GitHub</a>
+      <a href="https://twitter.com/TheTalesOfImmo" class="waves-effect waves-light btn blue-grey lighten-3 blue-grey-text text-darken-4" rel="me"><span class="sb-socicon socicon-twitter left"></span>Twitter</a>
+      <a href="https://www.facebook.com/sven.bischoff.393" class="waves-effect waves-light btn blue-grey lighten-3 blue-grey-text text-darken-4" rel="me"><span class="sb-socicon socicon-facebook left"></span>Facebook</a>
     </div>
     <div class="center-align">
-      <a href="https://500px.com/medienlampe" class="waves-effect waves-light btn blue-grey lighten-3 blue-grey-text text-darken-4"><span class="sb-socicon socicon-500px left"></span>500px</a>
-      <a href="http://www.last.fm/de/user/MultiGeneris" class="waves-effect waves-light btn blue-grey lighten-3 blue-grey-text text-darken-4"><span class="sb-socicon socicon-lastfm left"></span>LastFM</a>
-      <a href="https://steamcommunity.com/id/multigeneris" class="waves-effect waves-light btn blue-grey lighten-3 blue-grey-text text-darken-4"><span class="sb-socicon socicon-steam left"></span>Steam</a>
+      <a href="https://500px.com/medienlampe" class="waves-effect waves-light btn blue-grey lighten-3 blue-grey-text text-darken-4" rel="me"><span class="sb-socicon socicon-500px left"></span>500px</a>
+      <a href="http://www.last.fm/de/user/MultiGeneris" class="waves-effect waves-light btn blue-grey lighten-3 blue-grey-text text-darken-4" rel="me"><span class="sb-socicon socicon-lastfm left"></span>LastFM</a>
+      <a href="https://steamcommunity.com/id/multigeneris" class="waves-effect waves-light btn blue-grey lighten-3 blue-grey-text text-darken-4" rel="me"><span class="sb-socicon socicon-steam left"></span>Steam</a>
     </div>
     <div class="center-align">
-      <a href="mailto:info@bischoff.media?subject=Kontakt" class="waves-effect waves-light btn blue-grey lighten-3 blue-grey-text text-darken-4"><span class="sb-socicon socicon-mail left"></span>E-Mail</a>
-      <a href="https://www.xing.com/profile/Sven_Bischoff10" class="sb-scp-button waves-effect waves-light btn blue-grey lighten-3 blue-grey-text text-darken-4"><span class="sb-socicon socicon-xing left"></span>XING</a>
-      <a href="https://de.linkedin.com/in/svenbischoff" class="waves-effect waves-light btn blue-grey lighten-3 blue-grey-text text-darken-4"><span class="sb-socicon socicon-linkedin left"></span>LinkedIn</a>
+      <a href="mailto:info@bischoff.media?subject=Kontakt" class="waves-effect waves-light btn blue-grey lighten-3 blue-grey-text text-darken-4" rel="me"><span class="sb-socicon socicon-mail left"></span>E-Mail</a>
+      <a href="https://www.xing.com/profile/Sven_Bischoff10" class="sb-scp-button waves-effect waves-light btn blue-grey lighten-3 blue-grey-text text-darken-4" rel="me"><span class="sb-socicon socicon-xing left"></span>XING</a>
+      <a href="https://de.linkedin.com/in/svenbischoff" class="waves-effect waves-light btn blue-grey lighten-3 blue-grey-text text-darken-4" rel="me"><span class="sb-socicon socicon-linkedin left"></span>LinkedIn</a>
     </div>
   </div>
-  <footer class="page-footer blue-grey darken-3"> 
+  <footer class="page-footer blue-grey darken-3">
     <div class="footer-copyright blue-grey darken-4 blue-grey-text text-lighten-4 center-align">
       Impressum || Sven Bischoff | Arnoldstraße 2 | 04299 Leipzig
     </div>
