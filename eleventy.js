55ff0e05607f54835254a117650e639c0ea3b14a
Sven Bischoff <sven@medienlampe.de>
2020-07-12 14:13:34 +0200

    Finishes migration.

---
+++ eleventy.js
@@ -0,0 +1,7 @@
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
+  eleventyConfig.addFilter("eleventyNavigation", eleventyNavigationPlugin);
+
+};
