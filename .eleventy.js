13d5be6d4368008de38bd5f87a3b72c567bfd132
Sven Bischoff <sven@medienlampe.de>
2023-03-02 21:43:45 +0100

    Maintenance.

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,7 @@
 const eleventySass = require("eleventy-sass");
 
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addPlugin(eleventySass);
 
+module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(eleventySass, {
     compileOptions: {
       permalink: function(contents, inputPath) {
