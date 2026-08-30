c68a3c4ad239515d4b60ac64774cea1cd53e89a8
Sven Bischoff <sven@medienlampe.de>
2026-08-30 21:55:33 +0200

    Adds SEO optimizations.

---
+++ src/_data/site.js
@@ -0,0 +1,17 @@
+export default {
+  url: "https://bischoff.media",
+  name: "Sven Bischoff",
+  image: "/assets/img/sb.webp",
+  description:
+    "Sven Bischoff ist Softwareentwickler und Tech Lead aus Leipzig, aktuell Senior Engineering Manager bei der Invia Travel Germany GmbH (ab-in-den-urlaub.de). Persönliche Website mit Profilen bei GitHub, Fotografie, Printables, XING und LinkedIn.",
+  descriptionEnglish:
+    "Sven Bischoff is a software developer and tech lead from Leipzig, Germany, currently Senior Engineering Manager at Invia Travel Germany GmbH (ab-in-den-urlaub.de). Personal site with links to his GitHub, photography, Printables, XING and LinkedIn profiles.",
+  sameAs: [
+    "https://github.com/medienlampe",
+    "https://bischoff.photo",
+    "https://www.printables.com/de/social/339997-medienlampe/about",
+    "https://www.xing.com/profile/Sven_Bischoff10",
+    "https://de.linkedin.com/in/svenbischoff",
+    "https://steamcommunity.com/id/multigeneris"
+  ]
+};
